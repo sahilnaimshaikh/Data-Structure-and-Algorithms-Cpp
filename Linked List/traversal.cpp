e6865faf53220f9d08e47c56cdc64bef0ea31302
sahil <sahilshaikh2286@gmail.com>
2022-02-08 01:13:20 +0530

    LinkedList/Traversal

---
+++ Linked List/traversal.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+            
+         struct linked1{
+             int data;
+             struct linked1 * nextAddress;
+         };
+
+         void traverseLinkedList(struct linked1 * ptr){
+             while(ptr != NULL){
+                 cout<<ptr->data<<endl;
+                ptr = ptr->nextAddress;
+             }
+         }
+
+int main(){
+
+           struct linked1 * head; 
+           struct linked1 * first;
+
+           head = (struct linked1 *)malloc(sizeof(struct linked1)); 
+           first = (struct linked1 *)malloc(sizeof(struct linked1)); 
+        
+           head->data = 34;
+           head->nextAddress = first;
+
+           first->data = 354;
+           first->nextAddress = NULL;
+
+           traverseLinkedList(head); 
+return 0;
+}
