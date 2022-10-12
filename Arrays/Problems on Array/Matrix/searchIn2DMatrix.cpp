c0989fb25e96cead6d39100a34311bfe28521533
sahil <sahilshaikh2286@gmail.com>
2022-10-12 21:07:02 +0530

    Testing

--- Arrays/Problems on Array/Matrix/searchIn2DMatrix.cpp
+++ Arrays/Problems on Array/Matrix/searchIn2DMatrix.cpp
@@ -18,7 +18,7 @@ using namespace std;
           
 int main(){
           
-vector<vector<int>> matrix = {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
+vector<vector<int>> matrix = {{1,3,5,7},{10,11,16,20},{23,31,34,60}};
           
 cout<<searchMatrix(matrix, 3) ;  
       
